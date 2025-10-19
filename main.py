39fda385dc2cab0aefbebce8b2fa8e12baca69b9
Code Nest <maxgate15@gmail.com>
2025-10-19 20:54:20 +0000

    Add new_payment router and enable debug mode

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from typing import Union
 import models
 from database import engine
-from routers import auth, user, games, payment, notification, sms
+from routers import auth, user, games, payment, notification, sms, new_payment
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers import new_payment
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 origin = [
     "http://localhost:3000",
     "http://localhost:8000",
     "https://coral-app-l62hg.ondigitalocean.app",
-    "https://a1-tips.vercel.app",
-    "https://a1-tips.com",
-    "https://www.a1-tips.com",
+    "https://a1-tips.vercel.app"
+
 ]
 
 app.add_middleware(
@@ -32,3 +32,4 @@ app.include_router(games.router)
 app.include_router(payment.router)
 app.include_router(notification.router)
 app.include_router(sms.router)
+app.include_router(new_payment.router)
