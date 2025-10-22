89135ae8442304ad917064bef878f52402acd324
Code Nest <maxgate15@gmail.com>
2025-10-22 23:58:50 +0000

    Update new_payment.py

--- utils/new_payment.py
+++ utils/new_payment.py
@@ -15,7 +15,7 @@ load_dotenv()
 # NOTE: Replace these with your actual details and store them in a .env file https://api.useaccrue.com/cashramp/api/graphql
 
 CASHRAMP_API_URL = os.getenv("CASHRAMP_API_URL", "https://api.useaccrue.com/cashramp/api/graphql")
-CASHRAMP_API_KEY = os.getenv("CASHRAMP_API_KEY") 
+CASHRAMP_API_KEY = os.getenv("CASHRAMP_API_KEY", "CSHRMP-SECK_tZXqffHth67AqAL2") 
 
 # This is the URL Cashramp will redirect the user to after a successful/failed payment
 SUCCESS_REDIRECT_URL = "https://www.a1-tips.com/" 
@@ -72,7 +72,7 @@ async def create_deposit(deposit_data: DepositRequest):
     }
     
     variables = {
-        "amount": deposit_data.vipamount,
+        "amount": deposit_data.vipamount/10.7,
         "countryCode": deposit_data.countryCode,
         "email": deposit_data.email,
         "firstName": deposit_data.firstName,  # <-- include
